a66f2cc351a8299229bcc8d936adf82641af8a39
Saadh S <atextbooksituation@gmail.com>
2021-01-01 19:41:54 -0500

    This was done ages ago. There’s two folders. Mostly duped. Tried to de-dupe and minimize stuff. This is mostly what the sample web app was still

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Zoomie from './Zoomus';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  return(
+    <>
+      <script src="https://source.zoom.us/1.8.1/lib/vendor/jquery.min.js"></script>
+      <>
+        <Zoomie 
+          meetingNumber={ 9686065478 } 
+          userName="ssgear456@gmail.com" 
+          passWord="2401234" 
+          role={ 0 } 
+        />
+      </>
+    </>
+  ) 
+};
+
+ReactDom.render( <App />, document.getElementById( 'root' ) );
+
+export default App;
