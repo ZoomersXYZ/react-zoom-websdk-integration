9148f9b3ea9d54a8b5f32232186e3e1c109bdcbd
Saadh S <atextbooksituation@gmail.com>
2021-01-01 19:43:02 -0500

    Again this was done a long time ago. Whenever the update to sdk 1.8.1 happened. Skipping the launch screeen for now

--- src/Zoom.js
+++ src/Zoom.js
@@ -1,87 +1,124 @@
-import React, { Component } from 'react'
-
-import { ZoomMtg } from "@zoomus/websdk";
-
 // Add this, never use it client side in production
 const API_KEY = '';
 // Add this, never use it client side in production
 const API_SECRET = '';
-// This can be your Personal Meeting ID
-const MEETING_NUMBER = 1234567890;
+import React, { Component } from 'react';
+import { ZoomMtg } from "@zoomus/websdk";
+// import { TextField, Button, Card } from "@material-ui/core";
+import './Zoomus.css'
 
-const meetConfig = {
-    apiKey: API_KEY,
-    apiSecret: API_SECRET,
-    meetingNumber: MEETING_NUMBER,
-    userName: 'test user',
-    passWord: '',
-    leaveUrl: 'https://zoom.us',
-    role: 0
-};
+const zoomMeeting = document.getElementById('zmmtg-root');
 
-export default class Zoom extends Component {
-    state = {
-        meetingLaunched: false,
+
+class Zoomus extends Component {
+  constructor( props ) {
+    super( props )
+  
+    this.state = {
+      meetingLaunched: false,
+      apiKey: API_KEY,
+      apiSecret: API_SECRET,
+      meetingNumber: '4149821362',
+      userName: 'Mr Robot',
+      passWord: '',
+      leaveUrl: 'https://zoomers.xyz',
+      role: 1
     }
-
-    launchMeeting = () => {
-        
-        // change state of meeting
-        this.setState({ meetingLaunched: !this.state.meetingLaunched })
-
-        // generateSignature should only be used in development
-        ZoomMtg.generateSignature({
-            meetingNumber: meetConfig.meetingNumber,
-            apiKey: meetConfig.apiKey,
-            apiSecret: meetConfig.apiSecret,
-            role: meetConfig.role,
-            success(res) {
-                console.log('signature', res.result);
-                ZoomMtg.init({
-                    leaveUrl: 'http://www.zoom.us',
-                    success() {
-                        ZoomMtg.join(
-                            {
-                                meetingNumber: meetConfig.meetingNumber,
-                                userName: meetConfig.userName,
-                                signature: res.result,
-                                apiKey: meetConfig.apiKey,
-                                userEmail: 'email@gmail.com',
-                                passWord: meetConfig.passWord,
-                                success() {
-                                    console.log('join meeting success');
-                                },
-                                error(res) {
-                                    console.log(res);
-                                }
-                            }
-                        );
-                    },
-                    error(res) {
-                        console.log(res);
-                    }
-                });
-            }
-        });
-    }
+  };
+  
+  launchMeeting = () => {
     
-    componentDidMount() {
-        ZoomMtg.setZoomJSLib("https://source.zoom.us/1.7.0/lib", "/av");
-        ZoomMtg.preLoadWasm();
-        ZoomMtg.prepareJssdk();
-    }
+    this.setState( { 
+      meetingLaunched: !this.state.meetingLaunched 
+    } );
+    const meetConfig = {
+      meetingNumber: this.state.meetingNumber,
+      passWord: this.state.passWord,
+      userName: this.state.userName,
+      apiKey: this.state.apiKey,        
+    };
 
-    render() {
-        const { meetingLaunched} = this.state;
-        // Displays a button to launch the meeting when the meetingLaunched state is false
-        return (
-            <>
-                {!meetingLaunched ? 
-                    <button className="launchButton" onClick={this.launchMeeting}>Launch Meeting</button> 
-                : 
-                    <></>
+    ZoomMtg.generateSignature( {            
+      meetingNumber: this.state.meetingNumber,
+      passWord: this.state.passWord,
+      userName: this.state.userName,
+      apiKey: this.state.apiKey,
+      apiSecret: this.state.apiSecret,
+      role: this.state.role,
+      
+      success ( res )  {
+        console.log( 'signature', res.result );
+        ZoomMtg.init( {
+          leaveUrl: 'localhost', 
+          // isSupportAV: false, // messes display up
+          debug: true, 
+          disableJoinAudio: true, 
+          disableCallOut: true, 
+          disableRecord: true, 
+
+          success() {
+            ZoomMtg.join(                            
+              {
+                meetingNumber: meetConfig.meetingNumber,
+                userName: meetConfig.userName,
+                signature: res.result,
+                apiKey: meetConfig.apiKey,
+                userEmail: '',
+                passWord: meetConfig.passWord,
+                success() {
+                  console.log( 'Meeting Joined' );
+                },
+                error( res ) {
+                  console.log( res );
                 }
-            </>
-        )
+              }
+            );
+          },
+          error( res ) {
+            console.log( res );
+          } 
+        } );
+      }
+    } );
+  };
+  
+  componentDidMount() {
+    ZoomMtg.setZoomJSLib( 'https://source.zoom.us/1.8.1/lib', '/av' );
+    ZoomMtg.preLoadWasm();
+    ZoomMtg.prepareJssdk();
+
+    this.launchMeeting();
+  }
+
+  handleChange = ( e ) => {
+    this.setState( { [ e.target.name ]: e.target.value } )
+  }
+
+  render() {
+    if ( zoomMeeting != null ) {
+      zoomMeeting.style.height = '1920px';
+      zoomMeeting.style.width = '1080px';
+      zoomMeeting.style.position = 'relative';
+      zoomMeeting.style.zIndex = '1';
+      zoomMeeting.style.margin = '0%'
     }
+    // const { meetingLaunched, userName, meetingNumber, passWord } = this.state;
+    return (
+      <>
+        {/* { !meetingLaunched ?
+          <form className="form">
+            <TextField type="text" placeholder="Username" name="userName" value={ userName } onChange={ this.handleChange } />
+            <TextField type="text" placeholder="Meeting Number" name="meetingNumber" value={ meetingNumber } onChange={ this.handleChange } />
+            <TextField type="text" placeholder="Password" name="passWord" value={ passWord } onChange={ this.handleChange } />
+
+            <Button variant="contained" color="primary" className="launchButton" onClick={this.launchMeeting }>Launch Meeting</Button>
+          </form> 
+        :   
+          <></>
+        } */}
+      </>
+    )
+  }
 }
+
+export default Zoomus;
